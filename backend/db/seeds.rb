d86d70ebf06116ce37d74bc432497198278d1acc
Noah McMillan <noahm27@gmail.com>
2023-09-30 19:31:35 -0700

    Added seed example

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,37 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Seeding Data ..."
+
+## CATEGORIES
+
+puts "Finding or Creating Categories ..."
+
+cat1 = Category.find_or_create_by! name: 'food'
+cat2 = Category.find_or_create_by! name: 'drinks'
+
+puts "Re-creating Dishes ..."
+
+Dish.destroy_all
+
+cat1.dishes.create!({
+  name: 'steak',
+  description: 'yummy',
+  price: 500,
+  quantity: 5,
+  image_url: 'backend/app/assets/images/1_steakhouse_wings.jpg'
+})
+
+puts "Re-creating Users ..."
+
+User.destroy_all
+
+User.create({
+  first_name: 'Billy',
+  last_name: 'Bob',
+  username: 'BBcoolguy',
+  email: 'BillyB@gmail.com',
+  password: 'password123',
+  phone_number: '222-222-2222'
+})
+
+puts "DONE!"
