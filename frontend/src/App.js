cfc673a229a39632d85b6494c1ed374115c367b5
Colin Spangelo <colin.spangelo@gmail.com>
2023-10-02 17:54:06 -0600

    Fixed an icon error issue and commented out the DishList component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,10 @@ function App() {
 
   return (
     <div className="App">
-      {/* <UserList users={state.users} /> */}
+      <UserList users={state.users} />
       {/* <Twilio/> */}
       {/* <DishListItem dish={state}/> */}
-      <DishList dish={state}/>
+      {/* <DishList dish={state}/> */}
     </div>
   );
 }
