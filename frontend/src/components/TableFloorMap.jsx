2888fcb998336c3cd78eb46fc03c770aad03d4e7
= <steven.serruya@gmail.com>
2023-10-07 21:49:21 -0500

    add buttons for each table + css

--- frontend/src/components/TableFloorMap.jsx
+++ frontend/src/components/TableFloorMap.jsx
@@ -4,10 +4,15 @@ import { faCow, faBreadSlice, faShrimp, faEgg, faSeedling, faHeart, faCertificat
 
 const TableFloorMap = () => {
   const [isOn, setIsOn] = useState(false);
+  const [activeButtons, setActiveButtons] = useState({});
 
-  const handleToggle = () => {
-    setIsOn(prevIsOn => !prevIsOn);
+  const handleToggle = (buttonId) => {
+    setActiveButtons(prevState => ({
+      ...prevState,
+      [buttonId]: !prevState[buttonId]
+    }));
   };
+
 
   return (
     <div className="imageContainer">
@@ -17,13 +22,31 @@ const TableFloorMap = () => {
         {isOn ? 'On' : 'Off'}
       </button> */}
 
-      <i class="fa-solid fa-certificate but-1" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-2" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-3" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-4" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-5" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-6" onClick={handleToggle}></i>
-      <i class="fa-solid fa-certificate but-7" onClick={handleToggle}></i>
+      <i className={`fa-solid fa-certificate but-1 ${activeButtons[1] ? 'activeButton' : ''}`} onClick={() => handleToggle(1)}></i>
+      <i className={`fa-solid fa-certificate but-2 ${activeButtons[2] ? 'activeButton' : ''}`} onClick={() => handleToggle(2)}></i>
+      <i className={`fa-solid fa-certificate but-3 ${activeButtons[3] ? 'activeButton' : ''}`} onClick={() => handleToggle(3)}></i>
+      <i className={`fa-solid fa-certificate but-4 ${activeButtons[4] ? 'activeButton' : ''}`} onClick={() => handleToggle(4)}></i>
+      <i className={`fa-solid fa-certificate but-5 ${activeButtons[5] ? 'activeButton' : ''}`} onClick={() => handleToggle(5)}></i>
+      <i className={`fa-solid fa-certificate but-6 ${activeButtons[6] ? 'activeButton' : ''}`} onClick={() => handleToggle(6)}></i>
+      <i className={`fa-solid fa-certificate but-7 ${activeButtons[7] ? 'activeButton' : ''}`} onClick={() => handleToggle(7)}></i>
+      <i className={`fa-solid fa-certificate but-8 ${activeButtons[8] ? 'activeButton' : ''}`} onClick={() => handleToggle(8)}></i>
+      <i className={`fa-solid fa-certificate but-9 ${activeButtons[9] ? 'activeButton' : ''}`} onClick={() => handleToggle(9)}></i>
+      <i className={`fa-solid fa-certificate but-10 ${activeButtons[10] ? 'activeButton' : ''}`} onClick={() => handleToggle(10)}></i>
+      <i className={`fa-solid fa-certificate but-11 ${activeButtons[11] ? 'activeButton' : ''}`} onClick={() => handleToggle(11)}></i>
+      <i className={`fa-solid fa-certificate but-12 ${activeButtons[12] ? 'activeButton' : ''}`} onClick={() => handleToggle(12)}></i>
+      <i className={`fa-solid fa-certificate but-13 ${activeButtons[13] ? 'activeButton' : ''}`} onClick={() => handleToggle(13)}></i>
+      <i className={`fa-solid fa-certificate but-14 ${activeButtons[14] ? 'activeButton' : ''}`} onClick={() => handleToggle(14)}></i>
+      <i className={`fa-solid fa-certificate but-15 ${activeButtons[15] ? 'activeButton' : ''}`} onClick={() => handleToggle(15)}></i>
+      <i className={`fa-solid fa-certificate but-16 ${activeButtons[16] ? 'activeButton' : ''}`} onClick={() => handleToggle(16)}></i>
+      <i className={`fa-solid fa-certificate but-17 ${activeButtons[17] ? 'activeButton' : ''}`} onClick={() => handleToggle(17)}></i>
+      <i className={`fa-solid fa-certificate but-18 ${activeButtons[18] ? 'activeButton' : ''}`} onClick={() => handleToggle(18)}></i>
+
+
+
+
+
+
+
 
 
 
