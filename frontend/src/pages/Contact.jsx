f925c21620d306c60a123a20ce85ecc07a37a702
Noah McMillan <noahm27@gmail.com>
2023-10-10 05:18:04 -0700

    added feedback page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,25 +1,44 @@
 import React from "react";
+import "../styles/Contact.scss";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
 
   return (
-    <div>
+    <div className="contact">
       <h1>Contact Us</h1>
-      <section>
-        <h3>Feedback</h3>
-        <p>We continually strive to deliver the best possible experience for you, our valued guests, and we welcome your feedback, perspectives and ideas.</p>
-      </section>
+      <div className="contact-container">
 
-      <section>
-        <h3>Make a Reservation</h3>
-        <p>Please make a reservation online for the location you would like to visit.</p>
-      </section>
+        <section className="contact-section">
+          <div className="contact-text">
+            <h3>Feedback</h3>
+            <p>We continually strive to deliver the best possible experience for you, our valued guests, and we welcome your feedback, perspectives and ideas.</p>
+          </div>
+          <Link to="/feedback">
+            <button>Feedback</button>
+          </Link>
+        </section>
 
-      <section>
-        <h3>Media Inquiries</h3>
-        <p>Are you interested in sharing a media or advertising opportunity with us? Please email your contact information for our Marketing and Communications team to consider.</p>
-      </section>
+        <section className="contact-section">
+          <div className="contact-text">
+            <h3>Make a Reservation</h3>
+            <p>Please make a reservation online for the location you would like to visit.</p>
+          </div>
+          <Link to="/reservations">
+            <button>Reservations</button>
+          </Link>
+        </section>
 
+        <section className="contact-section">
+          <div className="contact-text">
+            <h3>Media Inquiries</h3>
+            <p>Are you interested in sharing a media or advertising opportunity with us? Please email your contact information for our Marketing and Communications team to consider.</p>
+          </div>
+          <Link to="/">
+            <button>Email Us</button>
+          </Link>
+        </section>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Feedback.jsx
+++ frontend/src/pages/Feedback.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import "../styles/Feedback.scss";
 
 const Feedback = () => {
 
-  return (
-    <div>
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert("Thank you for the feedback.");
+    window.location.reload(true);
+  };
 
+  return (
+    <div className="feedback">
+      <div className="feedback-container">
+        <h1>Your feedback is important to us</h1>
+        <p>If your experience was not up to the usual Charcoal standard, please leave a comment below so we can address it.</p>
+        <h3>A few details about yourself</h3>
+        <form className="feedback-form" onSubmit={onSubmit}>
+          <input type="text" name="fullName" placeholder="YOUR FIRST AND LAST NAME" required />
+          <input type="text" name="email" placeholder="EMAIL ADDRESS" required />
+          <textarea className="feedback-textarea" type="text" placeholder="ENTER YOUR COMMENTS" />
+          <button>SEND FEEDBACK</button>
+        </form>
+      </div>
     </div>
   );
 };
